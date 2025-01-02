f50885b783621b3045b4c485df29304bff912381
Flavio-02 <flaviogarrone.event@gmail.com>
2025-01-02 17:02:49 +0100

    creazione del file
    creato file Esercizi_Vacanze con diverse region

---
+++ Esercizi_Vacanze/Esercizi_Vacanze/Program.cs
@@ -0,0 +1,387 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Esercizi_Vacanze
+{
+    #region es 1
+
+    #endregion
+
+    #region
+    #endregion
+
+    #region
+    #endregion
+
+    #region
+    #endregion
+
+    #region
+    #endregion
+
+    #region
+    #endregion
+
+    #region
+    #endregion
+
+    #region
+    #endregion
+
+    #region
+    #endregion
+
+    #region
+    #endregion
+
+    #region
+    #endregion
+
+    #region
+    #endregion
+
+    #region
+    #endregion
+
+    #region
+    #endregion
+
+    #region
+    #endregion
+
+    #region
+    #endregion
+
+    #region
+    #endregion
+
+    #region
+    #endregion
+
+    #region
+    #endregion
+
+    #region
+    #endregion
+
+    #region
+    #endregion
+
+    #region
+    #endregion
+
+    #region
+    #endregion
+
+    #region
+    #endregion
+
+    #region
+    #endregion
+
+    #region
+    #endregion
+
+    #region
+    #endregion
+
+    #region
+    #endregion
+
+    #region
+    #endregion
+
+    #region
+    #endregion
+
+    #region
+    #endregion
+
+    #region
+    #endregion
+
+    #region
+    #endregion
+
+    #region
+    #endregion
+
+    #region
+    #endregion
+
+    #region
+    #endregion
+
+    #region
+    #endregion
+
+    #region
+    #endregion
+
+    #region
+    #endregion
+
+    #region
+    #endregion
+
+    #region
+    #endregion
+
+    #region
+    #endregion
+
+    #region
+    #endregion
+
+    #region
+    #endregion
+
+    #region
+    #endregion
+
+    #region
+    #endregion
+
+    #region
+    #endregion
+
+    #region
+    #endregion
+
+    #region
+    #endregion
+
+    #region
+    #endregion
+
+    #region
+    #endregion
+
+    #region
+    #endregion
+
+    #region
+    #endregion
+
+    #region
+    #endregion
+
+    #region
+    #endregion
+
+    #region
+    #endregion
+
+    #region
+    #endregion
+
+    #region
+    #endregion
+
+    #region
+    #endregion
+
+    #region
+    #endregion
+
+    #region
+    #endregion
+
+    #region
+    #endregion
+
+    #region
+    #endregion
+
+    #region
+    #endregion
+
+    #region
+    #endregion
+
+    #region
+    #endregion
+
+    #region
+    #endregion
+
+    #region
+    #endregion
+
+    #region
+    #endregion
+
+    #region
+    #endregion
+
+    #region
+    #endregion
+
+    #region
+    #endregion
+
+    #region
+    #endregion
+
+    #region
+    #endregion
+
+    #region
+    #endregion
+
+    #region
+    #endregion
+
+    #region
+    #endregion
+
+    #region
+    #endregion
+
+    #region
+    #endregion
+
+    #region
+    #endregion
+
+    #region
+    #endregion
+
+    #region
+    #endregion
+
+    #region
+    #endregion
+
+    #region
+    #endregion
+
+    #region
+    #endregion
+
+    #region
+    #endregion
+
+    #region
+    #endregion
+
+    #region
+    #endregion
+
+    #region
+    #endregion
+
+    #region
+    #endregion
+
+    #region
+    #endregion
+
+    #region
+    #endregion
+
+    #region
+    #endregion
+
+    #region
+    #endregion
+
+    #region
+    #endregion
+
+    #region
+    #endregion
+
+    #region
+    #endregion
+
+    #region
+    #endregion
+
+    #region
+    #endregion
+
+    #region
+    #endregion
+
+    #region
+    #endregion
+
+    #region
+    #endregion
+
+    #region
+    #endregion
+
+    #region
+    #endregion
+
+    #region
+    #endregion
+
+    #region
+    #endregion
+
+    #region
+    #endregion
+
+    #region
+    #endregion
+
+    #region
+    #endregion
+
+    #region
+    #endregion
+
+    #region
+    #endregion
+
+    #region
+    #endregion
+
+    #region
+    #endregion
+
+    #region
+    #endregion
+
+    #region
+    #endregion
+
+    #region
+    #endregion
+
+    #region
+    #endregion
+
+    #region
+    #endregion
+
+    #region
+    #endregion
+
+    #region
+    #endregion
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
